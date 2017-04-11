6708529f7530e1fbb765f6c5efe9dbcf9c661f6b
Holger Steinhaus <hsteinhaus@gmx.de>
2017-04-11 17:58:30 +0200

    AP_Frsky_Telem: transmit motor status flags

--- libraries/AP_Frsky_Telem/AP_Frsky_Telem.cpp
+++ libraries/AP_Frsky_Telem/AP_Frsky_Telem.cpp
@@ -27,10 +27,11 @@ extern const AP_HAL::HAL& hal;
 ObjectArray<mavlink_statustext_t> AP_Frsky_Telem::_statustext_queue(FRSKY_TELEM_PAYLOAD_STATUS_CAPACITY);
 
 //constructor
-AP_Frsky_Telem::AP_Frsky_Telem(AP_AHRS &ahrs, const AP_BattMonitor &battery, const RangeFinder &rng) :
+AP_Frsky_Telem::AP_Frsky_Telem(AP_AHRS &ahrs, const AP_BattMonitor &battery, const RangeFinder &rng, const AP_Motors* motors) :
     _ahrs(ahrs),
     _battery(battery),
-    _rng(rng)
+    _rng(rng),
+    _motors(motors)
     {}
 
 /*
@@ -255,8 +256,14 @@ void AP_Frsky_Telem::send_SPort(void)
                         case 1:
                             send_uint32(DATA_ID_TEMP1, _ap.control_mode); // send flight mode
                             break;
+                        case 2:
+                            if (_motors) {
+                                send_uint32(SMARTPORT_ID_ADC4, (_motors->limit.motor_upper << AP_MOTORS_MAX_NUM_MOTORS) | _motors->limit.motor_lower);
+                            }
+                            break;
+
                     }
-                    if (_SPort.various_call++ > 1) _SPort.various_call = 0;
+                    if (_SPort.various_call++ > 2) _SPort.various_call = 0;
                     break;
             }
             _SPort.sport_status = false;
